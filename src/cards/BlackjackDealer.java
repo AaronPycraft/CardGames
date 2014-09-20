39d0bc4bd831e9bc9d450e076feea1031e92522d
AaronPycraft <aapyc1@gmail.com>
2014-09-20 13:51:38 -0400

    Added metadata in comments of .java files

--- src/cards/BlackjackDealer.java
+++ src/cards/BlackjackDealer.java
@@ -1,4 +1,11 @@
 package cards;
+/* This class will perform the neccessary opertions that a dealer would perform
+* in the blackjack card game. 
+*/
+
+//--@author: Aaron Pycraft
+//--@version: 0.1
+//--@LastModified: 9/20/2014
 
 public class BlackjackDealer extends Dealer {
 
--- src/cards/Card.java
+++ src/cards/Card.java
@@ -1,4 +1,11 @@
 package cards;
+/* This class will define the operations and characteristics of a card object.
+*
+*/
+
+//--@author: Aaron Pycraft
+//--@version: 0.1
+//--@LastModified: 9/20/2014
 
 public class Card {
 
