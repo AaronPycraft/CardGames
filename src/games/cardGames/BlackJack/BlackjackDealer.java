9c4cc8beb5d6da01f04a40c584950a6b15082cc2
AaronPycraft <aapyc1@gmail.com>
2014-09-20 13:19:39 -0400

    Copied some items from my eclipse library, so i can use them in linux

---
+++ src/games/cardGames/BlackJack/BlackjackDealer.java
@@ -0,0 +1,5 @@
+package cards;
+
+public class BlackjackDealer extends Dealer {
+
+}
---
+++ src/games/cardGames/Dealer.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class Dealer {
+//--Fields
+	
+//--Constructor
+	
+//--Methods
+
+//--Exception Handling
+}
