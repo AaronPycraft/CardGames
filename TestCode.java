685076ffb58a1f541c1f61d76470f098154b670e
AaronPycraft <AaronPycraft@users.noreply.github.com>
2014-09-20 14:27:50 -0400

    Delete TestCode.java

--- TestCode.java
+++
@@ -1,8 +0,0 @@
-public class TestMain {
-	public static void main(String[] args) {
-		System.out.printline("Code works!");
-		//--Aaron was here.
-		//--Git can be a powerful tool. There are interactive tutorials
-		//  and such also.
-	}
-}
