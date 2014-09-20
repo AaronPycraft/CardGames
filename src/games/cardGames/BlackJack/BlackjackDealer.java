c8390f47f81293a8686b5ff883c67f2f13900c2a
AaronPycraft <aapyc1@gmail.com>
2014-09-20 14:11:29 -0400

    Began to layout class information

--- src/games/cardGames/BlackJack/BlackjackDealer.java
+++ src/games/cardGames/BlackJack/BlackjackDealer.java
@@ -1,5 +1,19 @@
 package cards;
-
+//--@author: Aaron Pycraft
+//--@version: 0.1
+//--@LastModified: 9/20/2014
 public class BlackjackDealer extends Dealer {
-
+//--Fields
+	private boolean isDealerPlaying;
+//--Constructor
+	public BlackjackDealer() {
+	}
+//--Methods
+	public void Deal() {
+	}
+	public void doPlayerTurn() {
+	}
+	public void getPlayerScore(Player player) {
+	}
+//--Exception Handling
 }
